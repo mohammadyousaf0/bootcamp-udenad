04c2687d49c3740cf47d07d27643d3bb98f5c522
mohammadyousaf0 <abdul2015@outlook.dk>
2021-08-17 14:02:53 +0200

    altsolution01a

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -1,7 +1,29 @@
-
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+
 document.addEventListener("DOMContentLoaded", () => {
+  /* document.getElementById("text-count").innerHTML = "(5)"; */
+  let number = 5;
+  let plusButton = document.querySelector("#btn-count-increase");
+  let minusButton = document.querySelector("#btn-count-decrease");
+  let textCountElement = document.querySelector("#text-count");
 
-	// skriv din kode her...
+  function updateNumber() {
+    textCountElement.textContent = number;
+  }
 
+  updateNumber();
+
+  plusButton.addEventListener("click", () => {
+    if (number < 10) {
+      number++;
+      updateNumber();
+    }
+  });
+
+  minusButton.addEventListener("click", () => {
+    if (number > 0) {
+      number--;
+      updateNumber();
+    }
+  });
 }); // Afslutter: DOMContentLoaded
