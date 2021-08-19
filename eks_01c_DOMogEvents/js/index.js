9e75237f107cadb458c8dc0518b718b8ecbe59cb
mohammadyousaf0 <abdul2015@outlook.dk>
2021-08-19 10:04:38 +0200

    Add solution to assignment 01c

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,6 +1,32 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+
 document.addEventListener("DOMContentLoaded", () => {
+  /* document.getElementById("text-count").innerHTML = "(5)"; */
+  let number = 5;
+  let maxValue = 10;
+  let minValue = 0;
+  let plusButton = document.querySelector("#btn-count-increase");
+  let minusButton = document.querySelector("#btn-count-decrease");
+  let textCountElement = document.querySelector("#text-count");
 
-	// skriv din kode her...
+  function updateNumber() {
+    textCountElement.textContent = number;
+  }
 
+  updateNumber();
+
+  function checkMinMaxValue(direction) {
+    if (direction === "inc" && number < maxValue) {
+      number++;
+      updateNumber();
+    }
+
+    if (direction === "dec" && number > minValue) {
+      number--;
+      updateNumber();
+    }
+  }
+
+  plusButton.addEventListener("click", () => checkMinMaxValue("inc"));
+  minusButton.addEventListener("click", () => checkMinMaxValue("dec"));
 }); // Afslutter: DOMContentLoaded
